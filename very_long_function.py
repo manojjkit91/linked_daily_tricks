975996c7597da85c097292ba8a8b221a12a143fe
Manoj Kumar <manoj.jkit91@gmail.com>
2022-08-14 01:45:11 +0900

    add pre-commit examples

---
+++ very_long_function.py
@@ -0,0 +1,13 @@
+def very_long_function(
+    long_variable_name1,
+    long_variable_name2,
+    long_variable_name3,
+    long_variable_name4,
+    long_variable_name5,
+    long_variable_name6,
+    long_variable_name7,
+    long_variable_name8,
+    long_variable_name9,
+    long_variable_name10,
+):
+    pass
